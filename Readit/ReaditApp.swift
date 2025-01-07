3b4c429e35f57e703417184ecb1b92cf69047916
Moutaz baaj <moutazbaag@gmail.com>
2025-01-07 19:16:35 +0100

    all tabs has its NavigationStack

--- Readit/ReaditApp.swift
+++ Readit/ReaditApp.swift
@@ -12,18 +12,29 @@ struct ReaditApp: App {
     var body: some Scene {
         WindowGroup {
             TabView {
-                HomeView()
-                    .tabItem {
-                        Label("Home", systemImage: "house")
-                    }
-                SettingsView()
-                    .tabItem {
-                        Label("My Library", systemImage: "gear")
-                    }
-                SettingsView()
-                    .tabItem {
-                        Label("More", systemImage: "gear")
-                    }
+                // Home Tab
+                NavigationStack {
+                    HomeView()
+                }
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+
+                // Library Tab
+                NavigationStack {
+                    LibraryView()
+                }
+                .tabItem {
+                    Label("My Library", systemImage: "books.vertical")
+                }
+
+                // Settings Tab
+                NavigationStack {
+                    SettingsView()
+                }
+                .tabItem {
+                    Label("More", systemImage: "ellipsis.circle")
+                }
             }
         }
     }
