c87f21762b8904aa8d70e65fce5c65ae9c130ad0
Moutaz baaj <moutazbaag@gmail.com>
2025-01-08 00:09:55 +0100

    language Enum added to allow the user to choose a language to read from

---
+++ Readit/Util/Language.swift
@@ -0,0 +1,64 @@
+//
+//  Language.swift
+//  Readit
+//
+//  Created by Moutaz Baaj on 07.01.25.
+//
+
+import Foundation
+
+enum Language: String, CaseIterable {
+    case english = "en-US"
+    case japanese = "ja-JP"
+    case french = "fr-FR"
+    case german = "de-DE"
+    case spanish = "es-ES"
+    case italian = "it-IT"
+    case chineseMandarin = "zh-CN"
+    case chineseHongKong = "zh-HK"
+    case chineseTaiwan = "zh-TW"
+    case korean = "ko-KR"
+    case arabic = "ar-SA"
+    case russian = "ru-RU"
+    case portugueseBrazil = "pt-BR"
+    case portuguesePortugal = "pt-PT"
+    case dutch = "nl-NL"
+    case swedish = "sv-SE"
+    case norwegian = "no-NO"
+    case danish = "da-DK"
+    case finnish = "fi-FI"
+    case polish = "pl-PL"
+    case greek = "el-GR"
+    case turkish = "tr-TR"
+    case thai = "th-TH"
+    case hindi = "hi-IN"
+    
+    var displayName: String {
+        switch self {
+        case .english: return "English (US) - English"
+        case .japanese: return "日本語 (Japanese)"
+        case .french: return "Français (French)"
+        case .german: return "Deutsch (German)"
+        case .spanish: return "Español (Spanish)"
+        case .italian: return "Italiano (Italian)"
+        case .chineseMandarin: return "中文 (普通话 - Chinese Mandarin)"
+        case .chineseHongKong: return "中文 (香港 - Chinese Hong Kong)"
+        case .chineseTaiwan: return "中文 (台灣 - Chinese Taiwan)"
+        case .korean: return "한국어 (Korean)"
+        case .arabic: return "العربية (Arabic)"
+        case .russian: return "Русский (Russian)"
+        case .portugueseBrazil: return "Português (Brasil - Portuguese Brazil)"
+        case .portuguesePortugal: return "Português (Portugal - Portuguese Portugal)"
+        case .dutch: return "Nederlands (Dutch)"
+        case .swedish: return "Svenska (Swedish)"
+        case .norwegian: return "Norsk (Norwegian)"
+        case .danish: return "Dansk (Danish)"
+        case .finnish: return "Suomi (Finnish)"
+        case .polish: return "Polski (Polish)"
+        case .greek: return "Ελληνικά (Greek)"
+        case .turkish: return "Türkçe (Turkish)"
+        case .thai: return "ไทย (Thai)"
+        case .hindi: return "हिंदी (Hindi)"
+        }
+    }
+}
