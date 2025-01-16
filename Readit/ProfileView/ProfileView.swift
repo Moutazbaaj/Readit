effd2a532512f529cb98e250f1606a8f5dc7b106
Moutazbaaj <moutazbaaj@gmail.com>
2025-01-16 21:11:58 +0100

    Color background

--- Readit/ProfileView/ProfileView.swift
+++ Readit/ProfileView/ProfileView.swift
@@ -13,7 +13,7 @@ struct ProfileView: View {
     
     // ViewModel instances for managing profile and reports
     @StateObject var profileViewModel = ProfileViewModel.shared // ViewModel for managing User Profile
-
+    
     // Environment object to access authentication-related data and methods
     @EnvironmentObject var authViewModel: AuthViewModel
     
@@ -37,15 +37,26 @@ struct ProfileView: View {
     
     var body: some View {
         NavigationStack {
-            Form {
-                // Section for displaying profile information
+            
+            ZStack {
+                // Place the gradient outside the Form to ensure it covers the entire background
+                LinearGradient(
+                    gradient: Gradient(colors: [.blue.opacity(0.3), .purple.opacity(0.3)]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .edgesIgnoringSafeArea(.all)  // Ensure it covers the entire screen
+                
+                // The form content
+                Form {
+                    // Section for displaying profile information
                     ZStack {
-                            // Outer circle with shadow
-                            Circle()
-                                .stroke(lineWidth: 5) // Adjust the thickness of the circle
-                                .foregroundColor(.blue) // The color of the circle
-                                .shadow(color: .white, radius: 10) // White shadow to create the "raised" effect
-                                .frame(width: 220, height: 220)
+                        // Outer circle with shadow
+                        Circle()
+                            .stroke(lineWidth: 5) // Adjust the thickness of the circle
+                            .foregroundColor(.blue) // The color of the circle
+                            .shadow(color: .white, radius: 10) // White shadow to create the "raised" effect
+                            .frame(width: 220, height: 220)
                         HStack {
                             Spacer()
                             // Display profile image (view only, no editing)
@@ -68,11 +79,10 @@ struct ProfileView: View {
                         .padding()
                     }
                     .padding()
-
+                    
                     // Display user information
                     HStack {
                         Text("Username: ")
-//                            .foregroundStyle(.white)
                             .shadow(color: .blue, radius: 0.5)
                         Text("\(String(authViewModel.user?.username ?? "unknown"))")
                             .foregroundColor(Color(from: authViewModel.user?.color ?? "black"))
@@ -82,7 +92,6 @@ struct ProfileView: View {
                     .padding(.vertical)
                     
                     Text("Birthday: \(authViewModel.user?.birthday ?? Date(), style: .date)")
-//                        .foregroundStyle(.white)
                         .shadow(color: .blue, radius: 0.5)
                         .padding(.vertical)
                     
@@ -96,99 +105,98 @@ struct ProfileView: View {
                         .shadow(color: .blue, radius: 0.5)
                         .padding(.vertical)
                     
-
-                                
-                // Section for ImpressumView navigation
-                Section {
-                    NavigationLink(destination: ImpressumView()) {
-                        HStack {
-                            Spacer()
-                            Text("About Us")
-                                .foregroundStyle(.blue)
-                                .shadow(color: .blue, radius: 10)
-                            Spacer()
+                    // Section for ImpressumView navigation
+                    Section {
+                        NavigationLink(destination: ImpressumView()) {
+                            HStack {
+                                Spacer()
+                                Text("About Us")
+                                    .foregroundStyle(.blue)
+                                    .shadow(color: .blue, radius: 10)
+                                Spacer()
+                            }
+                        }
+                    }
+                    
+                    // Section for logging out
+                    Section {
+                        Button(role: .destructive) {
+                            print("Logout button tapped")
+                            alertType = .logout
+                            showAlert = true
+                        } label: {
+                            HStack {
+                                Spacer()
+                                Text("Log Out")
+                                Spacer()
+                            }
+                        }
+                    }
+                    
+                    // Section for deleting the account
+                    Section {
+                        Button(role: .destructive) {
+                            print("Delete button tapped")
+                            alertType = .deleteAccount
+                            showAlert = true
+                        } label: {
+                            HStack {
+                                Spacer()
+                                Text("Delete Account")
+                                Spacer()
+                            }
                         }
                     }
                 }
-                
-                // Section for logging out
-                Section {
-                    Button(role: .destructive) {
-                        print("Logout button tapped")
-                        alertType = .logout
-                        showAlert = true
-                    } label: {
-                        HStack {
-                            Spacer()
-                            Text("Log Out")
-                            Spacer()
-                        }
+                .navigationBarItems(trailing:
+                                        Button(action: {
+                    showSettingSheet = true
+                    newUsername = authViewModel.user?.username ?? ""
+                    newBirthday = authViewModel.user?.birthday ?? Date()
+                    newColor = Color(from: authViewModel.user?.color ?? "")
+                }) {
+                    Image(systemName: "gearshape")
+                        .imageScale(.large)
+                }
+                )
+                .navigationTitle("Settings")
+                .navigationBarTitleDisplayMode(.inline)
+                .sheet(isPresented: $showSettingSheet) {
+                    ProfileSheetView(authViewModel: authViewModel, newUsername: $newUsername, newBirthday: $newBirthday, showSettingSheet: $showSettingSheet, selectedColor: $newColor)
+                        .presentationDetents([.large])
+                }
+                .alert(isPresented: $showAlert) {
+                    switch alertType {
+                    case .logout:
+                        return Alert(
+                            title: Text("Confirm Logout"),
+                            message: Text("Are you sure you want to log out?"),
+                            primaryButton: .destructive(Text("Log Out")) {
+                                print("Logout confirmed")
+                                authViewModel.logout()  // Log out the user
+                                authViewModel.checkAuth()  // Check authentication status
+                            },
+                            secondaryButton: .cancel {}
+                        )
+                    case .deleteAccount:
+                        return Alert(
+                            title: Text("Confirm Delete Account"),
+                            message: Text("Are you sure you want to delete your account? This action cannot be undone."),
+                            primaryButton: .destructive(Text("Delete Account")) {
+                                print("Delete confirmed")
+                                authViewModel.deleteUserData()  // Delete user data
+                                authViewModel.checkAuth()  // Check authentication status
+                            },
+                            secondaryButton: .cancel()
+                        )
+                    case .none:
+                        return Alert(title: Text("Error"), message: Text("An unknown error occurred"), dismissButton: .default(Text("OK")))
                     }
                 }
-                
-                // Section for deleting the account
-                Section {
-                    Button(role: .destructive) {
-                        print("Delete button tapped")
-                        alertType = .deleteAccount
-                        showAlert = true
-                    } label: {
-                        HStack {
-                            Spacer()
-                            Text("Delete Account")
-                            Spacer()
-                        }
-                    }
-                }
             }
-            .navigationBarItems(trailing:
-                                    Button(action: {
-                showSettingSheet = true
-                newUsername = authViewModel.user?.username ?? ""
-                newBirthday = authViewModel.user?.birthday ?? Date()
-                newColor = Color(from: authViewModel.user?.color ?? "")
-            }) {
-                Image(systemName: "gearshape")
-                    .imageScale(.large)
+            .onAppear {
+                profileViewModel.loadProfileImage()  // Load the current profile image
             }
-            )
-            .navigationTitle("Settings")
-            .navigationBarTitleDisplayMode(.inline)
-            .sheet(isPresented: $showSettingSheet) {
-                ProfileSheetView(authViewModel: authViewModel, newUsername: $newUsername, newBirthday: $newBirthday, showSettingSheet: $showSettingSheet, selectedColor: $newColor)
-                    .presentationDetents([.large])
-            }
-            .alert(isPresented: $showAlert) {
-                switch alertType {
-                case .logout:
-                    return Alert(
-                        title: Text("Confirm Logout"),
-                        message: Text("Are you sure you want to log out?"),
-                        primaryButton: .destructive(Text("Log Out")) {
-                            print("Logout confirmed")
-                            authViewModel.logout()  // Log out the user
-                            authViewModel.checkAuth()  // Check authentication status
-                        },
-                        secondaryButton: .cancel {}
-                    )
-                case .deleteAccount:
-                    return Alert(
-                        title: Text("Confirm Delete Account"),
-                        message: Text("Are you sure you want to delete your account? This action cannot be undone."),
-                        primaryButton: .destructive(Text("Delete Account")) {
-                            print("Delete confirmed")
-                            authViewModel.deleteUserData()  // Delete user data
-                            authViewModel.checkAuth()  // Check authentication status
-                        },
-                        secondaryButton: .cancel()
-                    )
-                case .none:
-                    return Alert(title: Text("Error"), message: Text("An unknown error occurred"), dismissButton: .default(Text("OK")))
-                }
-            }
-        }
-        .onAppear {
-            profileViewModel.loadProfileImage()  // Load the current profile image
         }
     }
 }
