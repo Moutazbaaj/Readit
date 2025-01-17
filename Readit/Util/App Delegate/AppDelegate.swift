645cc25f67c9bae9427f724ae1fca52bb7174d4b
Moutazbaaj <moutazbaaj@gmail.com>
2025-01-17 10:35:34 +0100

    supportedInterfaceOrientationsFor Portrait

--- Readit/Util/App Delegate/AppDelegate.swift
+++ Readit/Util/App Delegate/AppDelegate.swift
@@ -9,9 +9,11 @@
 import UIKit
 import FirebaseCore
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        FirebaseApp.configure()
-        return true
+//@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
     }
 }
