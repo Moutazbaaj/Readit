3ecf52bdb66abfdbd163370b3968a775b2676737
Moutazbaaj <moutazbaaj@gmail.com>
2025-01-22 16:27:23 +0100

    func to processImage

--- Readit/LibraryView/LibraryViewModel.swift
+++ Readit/LibraryView/LibraryViewModel.swift
@@ -12,11 +12,18 @@ import FirebaseAuth
 import CoreLocation
 import FirebaseStorage
 import AVFAudio
+import Vision
 
 
 class LibraryViewModel: ObservableObject {
     
     static let shared = LibraryViewModel()
+    
+    @Published var selectedImage: UIImage?
+    @Published var extractedText: String?
+//    @Published var showImagePicker = false
+
+
     
     @Published var texts : [FireText] = []
     @Published var libreries : [FireLibrary] = []
@@ -259,6 +266,38 @@ class LibraryViewModel: ObservableObject {
             }
         }
     }
+    
+    
+    func processImage(image: UIImage?) {
+        guard let image = image, let cgImage = image.cgImage else { return }
+        
+        // Specify the languages you want to recognize
+        let request = VNRecognizeTextRequest { request, error in
+            if let error = error {
+                print("Text recognition failed with error: \(error.localizedDescription)")
+                return
+            }
+            
+            if let results = request.results as? [VNRecognizedTextObservation] {
+                let recognizedText = results.compactMap { $0.topCandidates(1).first?.string }.joined(separator: " ")
+                DispatchQueue.main.async {
+                    self.extractedText = recognizedText
+                }
+            }
+        }
+        
+        // Set recognition languages
+        request.recognitionLanguages = ["en", "ar", "ja", "zh-Hans", "zh-Hant"] // English, Arabic, Japanese, Simplified and Traditional Chinese
+        
+        // Fallback to automatic language detection if needed
+        request.usesLanguageCorrection = true
+
+        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        DispatchQueue.global(qos: .userInitiated).async {
+            try? handler.perform([request])
+        }
+    }
+
     
     
     func readTextAloud(from library: FireLibrary, in language: Language) {
