e5849a86ffbe38eb6b6defffdd57dd02e7bd8731
Moutazbaaj <moutazbaaj@gmail.com>
2025-01-26 01:10:27 +0100

    layout edits

--- Readit/LibraryView/AddLibrarySheet.swift
+++ Readit/LibraryView/AddLibrarySheet.swift
@@ -10,10 +10,20 @@ import SwiftUI
 
 struct AddLibrarySheet: View {
     @StateObject var viewModel = LibraryViewModel.shared
+    @Environment(\.dismiss) var dismiss // Environment value for dismissing
 
     @Binding var newLibraryTitle: String
     var body: some View {
-        VStack(spacing: 20) {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [Color.blue.opacity(0.1), Color.purple.opacity(0.3)]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .blur(radius: 10)
+            .edgesIgnoringSafeArea(.all)
+            
+        VStack() {
             TextField("Enter library title", text: $newLibraryTitle)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .onChange(of: newLibraryTitle) {_, newValue in
@@ -23,23 +33,26 @@ struct AddLibrarySheet: View {
                     }
                 }
                 .padding()
-
+            
             Text("\(newLibraryTitle.count)/50 characters")
                 .font(.caption)
                 .foregroundColor(newLibraryTitle.count > 50 ? .red : .gray)
-
+            
             Button("Add") {
                 if !newLibraryTitle.isEmpty {
                     viewModel.createLibrary(libraryTitle: newLibraryTitle)
                     newLibraryTitle = ""
+                    dismiss()
                 }
             }
             .disabled(newLibraryTitle.isEmpty || newLibraryTitle.count > 50)
             .padding()
-            .background(newLibraryTitle.isEmpty || newLibraryTitle.count > 50 ? Color.gray : Color.blue)
+            .frame(maxWidth: .infinity)
+            .background(newLibraryTitle.isEmpty || newLibraryTitle.count > 50 ? Color.gray : Color.black.opacity(0.6))
             .foregroundColor(.white)
-            .cornerRadius(8)
+            .cornerRadius(20)
         }
         .padding()
+    }
     }
 }
