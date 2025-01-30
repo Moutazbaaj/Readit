d953c21873f328c50ada68453a2e86280b766ee4
Moutazbaaj <moutazbaaj@gmail.com>
2025-01-31 00:35:44 +0100

    Layout edit

--- Readit/Util/Voice/VoiceListPickerView.swift
+++ Readit/Util/Voice/VoiceListPickerView.swift
@@ -13,35 +13,52 @@ struct VoiceListPickerView: View {
     @Binding var selectedVoice: Voice
     @Binding var isPresented: Bool
     var language: Language
-
-//    let voices: [Voice] = Voice.voices(for: language.rawValue) // Get available voices
-
+    
+    //    let voices: [Voice] = Voice.voices(for: language.rawValue) // Get available voices
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(Voice.voices(for: language.rawValue), id: \.identifier) { voice in
-                    HStack {
-                        Text(voice.displayName)
-                            .foregroundColor(.primary)
-                        
-                        Spacer()
-                        
-                        if voice.identifier == selectedVoice.identifier {
-                            Image(systemName: "checkmark.circle.fill")
-                                .foregroundColor(.blue)
+        NavigationStack {
+            ZStack {
+                // Background gradient
+                LinearGradient(
+                    gradient: Gradient(colors: [.purple.opacity(0.3), .blue.opacity(0.3)]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .edgesIgnoringSafeArea(.all)
+                VStack {
+                    ScrollView {
+                    ForEach(Voice.voices(for: language.rawValue), id: \.identifier) { voice in
+                            HStack {
+                                Text(voice.displayName)
+                                    .foregroundColor(.primary)
+                                
+                                Spacer()
+                                
+                                if voice.identifier == selectedVoice.identifier {
+                                    Image(systemName: "checkmark.circle.fill")
+                                        .foregroundColor(.blue)
+                                }
+                            }
+                            .padding()
+                            .contentShape(Rectangle()) // Ensure the whole row is tappable
+                            .overlay{
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(Color.gray.opacity(0.3))
+                            }
+                            .onTapGesture {
+                                selectedVoice = voice
+                                //                        isPresented = false
+                            }
                         }
                     }
-                    .contentShape(Rectangle()) // Ensure the whole row is tappable
-                    .onTapGesture {
-                        selectedVoice = voice
-//                        isPresented = false
-                    }
+                    .padding()
                 }
+                .listStyle(.plain)
+                .navigationTitle("Select a Voice")
+                .presentationDetents([.medium, .large])
+                .presentationCornerRadius(50)
             }
-            .navigationTitle("Select a Voice")
-            .navigationBarItems(trailing: Button("Done") {
-                isPresented = false
-            })
         }
     }
 }
