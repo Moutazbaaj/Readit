277b481d481d5adfa7e8c65b4324679598ebdd9f
Moutazbaaj <moutazbaaj@gmail.com>
2025-01-31 00:35:34 +0100

    Layout edit

--- Readit/Util/Language/LanguagePickerView.swift
+++ Readit/Util/Language/LanguagePickerView.swift
@@ -13,39 +13,42 @@ struct LanguagePickerView: View {
     @Binding var isPresented: Bool
     
     var body: some View {
-        ZStack {
-            LinearGradient(
-                gradient: Gradient(colors: [Color.purple.opacity(0.3), Color.blue.opacity(0.3)]),
-                startPoint: .top,
-                endPoint: .bottom
-            )
-            .edgesIgnoringSafeArea(.all)
-        VStack {
-            Text("Select Language")
-                .font(.headline)
-                .padding()
-            
-            Picker("Language", selection: $selectedLanguage) {
-                ForEach(Language.allCases, id: \.self) { language in
-                    Text(language.displayName).tag(language)
+        NavigationStack {
+            ZStack {
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.purple.opacity(0.3), Color.blue.opacity(0.3)]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .edgesIgnoringSafeArea(.all)
+                VStack {
+//                    Text("Select Language")
+//                        .font(.headline)
+//                        .padding()
+//                    
+                    Picker("Language", selection: $selectedLanguage) {
+                        ForEach(Language.allCases, id: \.self) { language in
+                            Text(language.displayName).tag(language)
+                        }
+                    }
+                    .pickerStyle(WheelPickerStyle())
+                    .frame(height: 200)
+                    
+//                    Button("Done") {
+//                        isPresented = false
+//                    }
+//                    .font(.headline)
+//                    .padding()
+//                    .frame(maxWidth: .infinity)
+//                    .background(Color.black.opacity(0.6))
+//                    .foregroundColor(.white)
+//                    .cornerRadius(20)
+//                    .padding()
                 }
+                .navigationBarTitle("Select Language")
+                .presentationDetents([.medium])
+                .presentationCornerRadius(50)
             }
-            .pickerStyle(WheelPickerStyle())
-            .frame(height: 200)
-            
-            Button("Done") {
-                isPresented = false
-            }
-            .font(.headline)
-            .padding()
-            .frame(maxWidth: .infinity)
-            .background(Color.black.opacity(0.6))
-            .foregroundColor(.white)
-            .cornerRadius(20)
-            .padding()
         }
-        .presentationDetents([.medium])
-        .presentationCornerRadius(50)
     }
-}
 }
