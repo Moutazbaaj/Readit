d3577ca7d429d28b93f21d08eb710522fe6e261e
Moutazbaaj <moutazbaaj@gmail.com>
2025-01-30 20:53:36 +0100

    /VoiceListPickerView addec

---
+++ Readit/Util/Voice/VoiceListPickerView.swift
@@ -0,0 +1,47 @@
+//
+//  VoicePickerView.swift
+//  Readit
+//
+//  Created by Moutaz Baaj on 30.01.25.
+//
+
+
+import SwiftUI
+import AVFoundation
+
+struct VoiceListPickerView: View {
+    @Binding var selectedVoice: Voice
+    @Binding var isPresented: Bool
+    var language: Language
+
+//    let voices: [Voice] = Voice.voices(for: language.rawValue) // Get available voices
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(Voice.voices(for: language.rawValue), id: \.identifier) { voice in
+                    HStack {
+                        Text(voice.displayName)
+                            .foregroundColor(.primary)
+                        
+                        Spacer()
+                        
+                        if voice.identifier == selectedVoice.identifier {
+                            Image(systemName: "checkmark.circle.fill")
+                                .foregroundColor(.blue)
+                        }
+                    }
+                    .contentShape(Rectangle()) // Ensure the whole row is tappable
+                    .onTapGesture {
+                        selectedVoice = voice
+//                        isPresented = false
+                    }
+                }
+            }
+            .navigationTitle("Select a Voice")
+            .navigationBarItems(trailing: Button("Done") {
+                isPresented = false
+            })
+        }
+    }
+}
