a2c52a10d4396a77fd5f6f0e75e0c5df06aa6640
Moutazbaaj <moutazbaaj@gmail.com>
2025-02-02 23:27:16 +0100

    Collection, your mother restructured to adopt to the text to speech manager

--- Readit/CollectionView/CollectionViewModel.swift
+++ Readit/CollectionView/CollectionViewModel.swift
@@ -11,6 +11,7 @@ import FirebaseFirestore
 import FirebaseAuth
 import AVFAudio
 import Vision
+import SwiftUI
 
 
 class CollectionViewModel: ObservableObject {
@@ -19,7 +20,7 @@ class CollectionViewModel: ObservableObject {
     
     @Published var selectedImage: UIImage?
     @Published var extractedText: String?
-
+    @Published var currentWordRange: NSRange? = nil
     
     @Published var texts : [FireText] = []
     @Published var libreries : [FireLibrary] = []
@@ -33,16 +34,20 @@ class CollectionViewModel: ObservableObject {
     
     // Firestore instance.
     private let firebaseFirestore = Firestore.firestore()
-
     
+    private var textToSpeechManager = TextToSpeechManager.shared
     
     private let synthesizer = AVSpeechSynthesizer()
-
+    
     
     init() {
         self.fetchLibraries()
         self.fetchFavLibraries()
+        textToSpeechManager.$currentWordRange
+            .receive(on: RunLoop.main)
+            .assign(to: &$currentWordRange)
     }
+    
     
     // Creates a new librery.
     func createLibrary(libraryTitle: String) {
@@ -303,48 +308,66 @@ class CollectionViewModel: ObservableObject {
         }
     }
     
-    func readTextAloudForLibrary(from library: FireLibrary, in language: Language, using voice: Voice) {
-        guard !texts.isEmpty else {
-            print("No texts available to read aloud.")
-            return
-        }
+//    func readTextAloudForLibrary(from library: FireLibrary, in language: Language, using voice: Voice) {
+//        guard !texts.isEmpty else {
+//            print("No texts available to read aloud.")
+//            return
+//        }
+//
+//        // Filter and prepare texts to read
+//        let textsToRead = texts
+//            .filter { $0.libraryId == library.id } // Ensure the texts belong to the specified library
+//            .sorted(by: {
+//                $0.timestamp.dateValue() < $1.timestamp.dateValue() // Sort by timestamp in ascending order
+//            })
+//            .enumerated() // Enumerate to get both index and content
+//            .map { index, text in
+//                "\(language.pageTranslation) \(index + 1). \(text.text)" // Add "Page X" prefix before each text
+//            }
+//            .joined(separator: ". ") // Concatenate texts with a separator
+//
+//        let utterance = AVSpeechUtterance(string: textsToRead)
+//        
+//        // Configure voice
+//        if let customVoice = AVSpeechSynthesisVoice(identifier: voice.identifier) {
+//            utterance.voice = customVoice
+//        } else {
+//            utterance.voice = AVSpeechSynthesisVoice(language: language.rawValue) // Fallback to language-based voice
+//        }
+//
+//        // Configure audio session
+//        do {
+//            let audioSession = AVAudioSession.sharedInstance()
+//            try audioSession.setCategory(.playback, mode: .default, options: [])
+//            try audioSession.setActive(true)
+//        } catch {
+//            print("Failed to configure audio session: \(error.localizedDescription)")
+//        }
+//
+//        synthesizer.speak(utterance)
+//    }
+    
+//    func stopSpeaking() {
+//        if synthesizer.isSpeaking {
+//            synthesizer.stopSpeaking(at: .immediate)
+//        }
+//    }
+    
+    
+    func readTextAloud(form text: String) {
+        textToSpeechManager.readTextAloud(from: text)
+    }
 
-        // Filter and prepare texts to read
-        let textsToRead = texts
-            .filter { $0.libraryId == library.id } // Ensure the texts belong to the specified library
-            .sorted(by: {
-                $0.timestamp.dateValue() < $1.timestamp.dateValue() // Sort by timestamp in ascending order
-            })
-            .enumerated() // Enumerate to get both index and content
-            .map { index, text in
-                "\(language.pageTranslation) \(index + 1). \(text.text)" // Add "Page X" prefix before each text
-            }
-            .joined(separator: ". ") // Concatenate texts with a separator
-
-        let utterance = AVSpeechUtterance(string: textsToRead)
-        
-        // Configure voice
-        if let customVoice = AVSpeechSynthesisVoice(identifier: voice.identifier) {
-            utterance.voice = customVoice
-        } else {
-            utterance.voice = AVSpeechSynthesisVoice(language: language.rawValue) // Fallback to language-based voice
-        }
-
-        // Configure audio session
-        do {
-            let audioSession = AVAudioSession.sharedInstance()
-            try audioSession.setCategory(.playback, mode: .default, options: [])
-            try audioSession.setActive(true)
-        } catch {
-            print("Failed to configure audio session: \(error.localizedDescription)")
-        }
-
-        synthesizer.speak(utterance)
+    
+    func readTextAloudForLibrary(from library: FireLibrary) {
+        textToSpeechManager.readTextAloudForLibrary(from: library, from: texts)
     }
     
     func stopSpeaking() {
-        if synthesizer.isSpeaking {
-            synthesizer.stopSpeaking(at: .immediate)
-        }
+        textToSpeechManager.stopSpeaking()
+    }
+    
+    func highlightedText(_ fullText: String) -> Text {
+        return textToSpeechManager.highlightedText(fullText)
     }
 }
